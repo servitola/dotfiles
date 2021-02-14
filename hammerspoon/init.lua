37188b292b9b10076df33cd0fc43225791d33ffe
servitola <servitola@gmail.com>
2021-02-15 01:35:06 +0200

    [Hammerspoon] Fix

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,6 +1,7 @@
 hs.loadSpoon("window")
 hs.window.animationDuration = 0.08
-spoon.WindowsManager:bindWindowsHotkeys({
+
+spoon.window:bindWindowsHotkeys({
     up = {{"left_control", "left_option"}, "up"},
     right = {{"left_control", "left_option"}, "right"},
     down = {{"left_control", "left_option"}, "down"},
