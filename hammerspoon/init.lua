a749e8e66496ddfa4b8cad6dc20f592a2982bdf6
servitola <servitola@gmail.com>
2021-02-15 23:10:37 +0200

    [Hammerspoon] Refactor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,11 +1,21 @@
 hs.loadSpoon("window")
 hs.window.animationDuration = 0.08
 
+local hyper = {
+    "right_command", 
+    "right_control", 
+    "right_option", 
+    "right_shift"}
+
 spoon.window:bindWindowsHotkeys({
-    up = {{"left_control", "left_option"}, "up"},
-    right = {{"left_control", "left_option"}, "right"},
-    down = {{"left_control", "left_option"}, "down"},
-    left = {{"left_control", "left_option"}, "left"}
+    up = {hyper, "up"},
+    right = {hyper, "right"},
+    down = {hyper, "down"},
+    left = {hyper, "left"}
 })
 
 require "capslock";
+
+data = hs.json.read("../data/data.json")
+
+--hs.logger:w("test")
