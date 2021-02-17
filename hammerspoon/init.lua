9751e3d3b2e80a311ae670cc4594511d04caeaa8
servitola <servitola@gmail.com>
2021-02-17 09:29:15 +0200

    [Hammerspoon] Refactor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -14,8 +14,15 @@ spoon.window:bindWindowsHotkeys({
     left = {hyper, "left"}
 })
 
+spoon.window:bindWindowsHotkeys({
+    up = {{"left_control", "left_option"}, "up"},
+    right = {{"left_control", "left_option"}, "right"},
+    down = {{"left_control", "left_option"}, "down"},
+    left = {{"left_control", "left_option"}, "left"}
+})
+
 require "capslock";
 
-data = hs.json.read("../data/data.json")
+--data = hs.json.read("../data/data.json")
 
 --hs.logger:w("test")
