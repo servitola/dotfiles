6f9fc2ed2e7f8d58c8d83aa6998d23526ad221e3
servitola <servitola@gmail.com>
2021-02-18 09:15:59 +0200

    [Hammerspoon] Disable since errors

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,28 +1,28 @@
-hs.loadSpoon("window")
-hs.window.animationDuration = 0.08
+-- hs.loadSpoon("window")
+-- hs.window.animationDuration = 0.08
 
-local hyper = {
-    "right_command", 
-    "right_control", 
-    "right_option", 
-    "right_shift"}
+-- local hyper = {
+--     "right_command", 
+--     "right_control", 
+--     "right_option", 
+--     "right_shift"}
 
-spoon.window:bindWindowsHotkeys({
-    up = {hyper, "up"},
-    right = {hyper, "right"},
-    down = {hyper, "down"},
-    left = {hyper, "left"}
-})
+-- spoon.window:bindWindowsHotkeys({
+--     up = {hyper, "up"},
+--     right = {hyper, "right"},
+--     down = {hyper, "down"},
+--     left = {hyper, "left"}
+-- })
 
-spoon.window:bindWindowsHotkeys({
-    up = {{"left_control", "left_option"}, "up"},
-    right = {{"left_control", "left_option"}, "right"},
-    down = {{"left_control", "left_option"}, "down"},
-    left = {{"left_control", "left_option"}, "left"}
-})
+-- spoon.window:bindWindowsHotkeys({
+--     up = {{"left_control", "left_option"}, "up"},
+--     right = {{"left_control", "left_option"}, "right"},
+--     down = {{"left_control", "left_option"}, "down"},
+--     left = {{"left_control", "left_option"}, "left"}
+-- })
 
-require "capslock";
+-- require "capslock";
 
---data = hs.json.read("../data/data.json")
+-- --data = hs.json.read("../data/data.json")
 
---hs.logger:w("test")
+-- --hs.logger:w("test")
