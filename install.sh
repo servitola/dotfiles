dcaa7dcfa25d9968ed6fd86f32700b84cbfd7b98
servitola <servitola@gmail.com>
2021-02-20 14:02:32 +0200

    [Repository] Install script is in progres

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 # printf 'Json reading'
 # jq 'map(.caps_lock[0].key)' ./data/data.json
-brew bundle dump --force --file=.brewfile
+echo 'test'
+brew bundle dump --force --file='~/projects/pc-scripts/homebrew/.brewfile'
+pause
