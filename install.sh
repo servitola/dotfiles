533f4966906781a52d24d7c1be5b9a38aca8ecde
servitola <servitola@gmail.com>
2021-02-20 10:11:32 +0200

    [Repositpry] Clean stuff

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
-printf 'Json reading'
-jq 'map(.caps_lock[0].key)' ./data/data.json
+#!/usr/bin/env bash
+# printf 'Json reading'
+# jq 'map(.caps_lock[0].key)' ./data/data.json
+brew bundle dump --force --file=.brewfile
--- mac setup scripts/set-defaults.sh
+++ mac setup scripts/set-defaults.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
-# Sets reasonable macOS defaults.
-#
-# Or, in other words, set shit how I like in macOS.
-#
-# The original idea (and a couple settings) were grabbed from:
-#   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
-#
-# Run ./set-defaults.sh and you'll be good to go.
 
 # Close any open System Preferences panes, to prevent them from overriding
 # settings we’re about to change
