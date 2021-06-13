d2ea252f95e680929b5788518415f9d327fe6a81
servitola <servitola@gmail.com>
2021-06-13 08:21:08 +0300

    [Hammerspoon] Mouse clicks are in progress

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -111,7 +111,7 @@ end
 --     spoon.ModalMgr.supervisor:bind(hsappM_keys[1], hsappM_keys[2], "Enter AppM Environment", function()
 --         spoon.ModalMgr:deactivateAll()
 --         -- Show the keybindings cheatsheet once appM is activated
---         spoon.ModalMgr:activate({"appM"}, "#FFBD2E", true)
+--         spoon.ModalMgr:activate({"appM"}, "#FFBD2E", true)\
 --     end)
 -- end
 
@@ -252,20 +252,38 @@ end
 configFileWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig)
 configFileWatcher:start()
 
-function changeVolume(diff)
-    return function()
-        local current = hs.audiodevice.defaultOutputDevice():volume()
-        local new = math.min(100, math.max(0, math.floor(current + diff)))
-        if new > 0 then
-            hs.audiodevice.defaultOutputDevice():setMuted(false)
-        end
-        hs.alert.closeAll(0.0)
-        hs.alert.show("Volume " .. new .. "%", {}, 0.5)
-        hs.audiodevice.defaultOutputDevice():setVolume(new)
-    end
-end
+-- function changeVolume(diff)
+--     return function()
+--         local current = hs.audiodevice.defaultOutputDevice():volume()
+--         local new = math.min(100, math.max(0, math.floor(current + diff)))
+--         if new > 0 then
+--             hs.audiodevice.defaultOutputDevice():setMuted(false)
+--         end
+--         hs.alert.closeAll(0.0)
+--         hs.alert.show("Volume " .. new .. "%", {}, 0.5)
+--         hs.audiodevice.defaultOutputDevice():setVolume(new)
+--     end
+-- end
 
--- hs.hotkey.bind({'cmd'}, 'Down', changeVolume(-3))
--- hs.hotkey.bind({'cmd'}, 'Up', changeVolume(3))
+-- function clickMouseLeft()
+--     local clickState = hs.eventtap.event.properties.mouseEventClickState
+--     local point = hs.mouse.getAbsolutePosition()
+--     hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseDown, point):post()
+--     -- hs.usleep(1000)
+--     --hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseUp, point):post()
+--     -- hs.eventtap.event.newMouseEvent(hs.eventtap.event.types["leftMouseDown"], point):setProperty(clickState, 1):post()
+--     -- hs.eventtap.event.newMouseEvent(hs.eventtap.event.types["leftMouseUp"], point):setProperty(clickState, 1):post()
+-- end
+
+-- function clickMouseLeftTwice()
+--     clickMouseLeft()
+--     clickMouseLeft()
+-- end
+
+-- --hs.hotkey.bind(ctrlAndAlt, 'r', changeVolume(-3))
+-- hs.hotkey.bind(ctrlAndAlt, 'e', function() clickMouseLeft() end) 
+-- hs.hotkey.bind(ctrlAndAlt, 'q', function() clickMouseLeftTwice() end) 
+
+
 
 require "Configs/config_FadeLogo";
