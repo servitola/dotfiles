b6cdb86920fab4822274b633d545925767e33818
servitola <servitola@gmail.com>
2022-06-26 17:08:14 +0300

    [Hammerspoon] Press hyper + f on double left control

--- hammerspoon/Configs/constants.lua
+++ hammerspoon/Configs/constants.lua
@@ -1,3 +1,4 @@
 hyper = {"right_command", "right_control", "right_option", "right_shift"}
 hyperAndShift = {"left_shift", "right_command", "right_control", "right_option", "right_shift"}
 ctrlAndAlt = {"left_control", "left_option"}
+leftCtrlKeyCode = 59 
