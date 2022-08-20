eecb5603a4e56b92725dccf718feb4f4caaa2a0f
servitola <servitola@gmail.com>
2022-08-21 01:47:44 +0300

    [dotfiles] Adjust install script

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 echo 'installing'
 brew bundle dump --force --file='~/projects/pc-scripts/homebrew/.brewfile'
-ln -s ~/projects/pc-scripts/visual\ studio\ code/User ~/Library/Application\ Support/Code/User
+#FIX ln -s ~/projects/pc-scripts/visual\ studio\ code/User ~/Library/Application\ Support/Code/User
 ln -s ~/projects/pc-scripts/zsh/zshrc ~/.zshrc
 ln -s ~/projects/pc-scripts/git/gitconfig ~/.gitconfig
-pause
+sh "./mac setup scripts/set-defaults.sh"
