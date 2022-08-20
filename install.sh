d26dcea2b576f9b68f337e1813ca1f8b3fb2c9a5
servitola <servitola@gmail.com>
2022-08-21 01:19:01 +0300

    [Zsh] Adjust folder setup

--- install.sh
+++ install.sh
@@ -2,6 +2,6 @@
 echo 'installing'
 brew bundle dump --force --file='~/projects/pc-scripts/homebrew/.brewfile'
 ln -s ~/projects/pc-scripts/visual\ studio\ code/User ~/Library/Application\ Support/Code/User
-ln -s ~/projects/pc-scripts/zsh/.zshrc ~/.zshrc
+ln -s ~/projects/pc-scripts/zsh/zshrc ~/.zshrc
 ln -s ~/projects/pc-scripts/git/gitconfig ~/.gitconfig
 pause
