2da401fc8b45b4aec946e70aad0fa1a11110a674
servitola <servitola@gmail.com>
2022-08-20 23:18:40 +0300

    [Hammerspoon] Refactor scripts

--- hammerspoon/Spoons/HotKeys.spoon/init.lua
+++ hammerspoon/Spoons/HotKeys.spoon/init.lua
@@ -7,24 +7,6 @@ function obj:registerHotKey(modifiers, button, appName)
     end)
 end
 
--- function obj:openApp(name)
---     local logger = hs.logger.new("capslock", 'verbose')
---     logger.d()
---     logger.d('Open App:')
---     logger.d(name)
-
---     local app = hs.application.get(name)
---     if app then
---         if app:isFrontmost() then
---             app:hide()
---         else
---             app:mainWindow():focus()
---         end
---     else
---         hs.application.launchOrFocus(name)
---     end
--- end
-
 function obj:openApp(name)
     local logger = hs.logger.new("capslock", 'verbose')
     local app = hs.application.find(name)
@@ -34,40 +16,8 @@ function obj:openApp(name)
     elseif hs.application.frontmostApplication() ~= app then
       app:activate()
     else
-        -- if name == "Finder" then
-        --     logger.d("Finder")
-        --     local windows = app:allWindows()
-        --     if windows then
-        --         if table.getn(windows) ~= 0 then
-        --             logger.d("with windows")
-        --             app:hide()
-        --         end
-        --     else
-        --         logger.d("without windows")
-        --         hs.eventtap.keyStroke({'cmd'}, 'n')
-        --     end
-        -- else
-        --     logger.d("regular hide")
-            app:hide()
-        --end
+      app:hide()
     end
-
-    -- if name == "Finder" then
-    --     logger.d("Finder 2")
-    --     local windows = app:allWindows()
-    --     if windows ~= nil then
-    --         if table.getn(windows) ~= 0 then
-    --             logger.d("with windows 2")
-    --             --app:hide()
-    --         end
-    --     else
-    --         logger.d("without windows 2")
-    --         hs.eventtap.keyStroke({'cmd'}, 'n')
-    --     end
-    -- else
-    --     logger.d("regular hide 2")
-    --     --app:hide()
-    -- end
   end
 
 function obj:setup(modifier, app_list)
@@ -84,18 +34,6 @@ function obj:bindOpenApp(modifier, key, app_name)
     hs.hotkey.bind(modifier, key, function()
         obj:openApp(app_name)
     end)
-end
-
-function obj:init()
-    -- hs.hotkey.bind(hyper, '[', function()
-    --     hs.itunes.previous()
-    -- end)
-    -- hs.hotkey.bind(hyper, ']', function() 
-    --     hs.itunes.next()
-    -- end)
-    -- hs.hotkey.bind(hyper, 'space', function()
-    --     hs.itunes.playpause()
-    -- end)
 end
 
 return obj
--- hammerspoon/User/constants.lua
+++ hammerspoon/User/constants.lua
@@ -1,4 +1,5 @@
 hyper = {"right_command", "right_control", "right_option", "right_shift"}
 hyperAndShift = {"left_shift", "right_command", "right_control", "right_option", "right_shift"}
 ctrlAndAlt = {"left_control", "left_option"}
+--ctrlAndShift {"left_control", "left_shift"}
 leftCtrlKeyCode = 59 
