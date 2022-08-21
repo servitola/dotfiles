dabed5e9d288bfcc0d19c5565a7dc4478acd2d94
servitola <servitola@gmail.com>
2022-08-21 07:03:00 +0300

    [Zsh] Generate new settings for powerline

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 sudo -v
 echo 'installing'
 brew bundle dump --force --file='~/projects/pc-scripts/homebrew/.brewfile'
-#FIX ln -s ~/projects/pc-scripts/visual\ studio\ code/User ~/Library/Application\ Support/Code/User
+ln -s ~/projects/pc-scripts/visual\ studio\ code/User/ ~/Library/Application\ Support/Code/User/
 ln -s ~/projects/pc-scripts/zsh/zshrc ~/.zshrc
 ln -s ~/projects/pc-scripts/git/gitconfig ~/.gitconfig
 sh "./mac setup scripts/set-defaults.sh"
