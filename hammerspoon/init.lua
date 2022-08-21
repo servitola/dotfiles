085c0018520c856d43b11649edb99626bd7cda65
servitola <servitola@gmail.com>
2022-08-21 18:58:14 +0300

    [Hammerspoon] Refactor. Add new shortcuts

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -2,14 +2,13 @@ hs.console.clearConsole()
 hs.console.consoleFont({ name = 'Meslo LG M for Powerline', size = 20.0 })
 
 require "User/Configs/config_SpoonInstall";
+spoon.SpoonInstall:andUse("Windows");
+require "User/register_all_shortcuts"
+require "User/Configs/config_Popup_TranslateSelection";
 require "User/Configs/config_UrlDispatcher";
 require "User/Configs/config_WiFiTransitions";
-require "User/Configs/config_Popup_TranslateSelection";
-spoon.SpoonInstall:andUse("Windows");
 spoon.SpoonInstall:andUse("HoldToQuit", { start = true});
 spoon.SpoonInstall:andUse("KSheet");
-spoon.SpoonInstall:andUse("UnsplashZ");
-require "User/register_all_shortcuts"
-require "User/reload_hammerspoon_on_config_change"
-
+spoon.SpoonInstall:andUse("UnsplashZ"); 
+spoon.SpoonInstall:andUse("ReloadConfiguration", { start = true });
 spoon.SpoonInstall:andUse("FadeLogo", { start = true})
