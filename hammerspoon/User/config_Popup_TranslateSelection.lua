9cfefe4b9fd3f8d45c94356db51254a137490b7d
servitola <servitola@gmail.com>
2022-08-30 00:52:17 +0300

    [Hammerspoon] Still working

--- hammerspoon/User/config_Popup_TranslateSelection.lua
+++ hammerspoon/User/config_Popup_TranslateSelection.lua
@@ -1,5 +1,5 @@
 local wm = hs.webview.windowMasks
-spoon.SpoonInstall:andUse("PopupTranslateSelection", {
+hs.spoons.use("PopupTranslateSelection", {
     config = {
         popup_style = wm.utility | wm.HUD | wm.titled | wm.closable | wm.resizable
     },
@@ -7,5 +7,4 @@ spoon.SpoonInstall:andUse("PopupTranslateSelection", {
         translate_to_ru = {{"left_control", "left_option"}, "r"},
         translate_to_en = {{"left_control", "left_option"}, "e"},
         translate_to_el = {{"left_control", "left_option"}, "g"},
-    }
-})
+    }}, true)
