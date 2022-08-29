b9720c589bfe9ffc65c86d542c7746fbb51ba518
servitola <servitola@gmail.com>
2022-08-29 21:33:36 +0300

    [Hammerspoon] Refactor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,7 +4,6 @@ hs.console.consoleFont({ name = 'Meslo LG M for Powerline', size = 20.0 })
 hs.loadSpoon("SpoonInstall")
 spoon.SpoonInstall:andUse("Windows");
 require "User/register_all_shortcuts"
-require "User/config_usb"
 require "User/config_Popup_TranslateSelection";
 require "User/config_UrlDispatcher";
 spoon.SpoonInstall:andUse("KSheet");
