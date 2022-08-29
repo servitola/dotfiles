d2088b5ff11f1dbf36a8920df79b6422ba23e298
servitola <servitola@gmail.com>
2022-08-30 01:11:33 +0300

    [Hammerspoon] Refactor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -4,8 +4,12 @@ hs.console.consoleFont({ name = 'Meslo LG M for Powerline', size = 20.0 })
 hs.loadSpoon("UnsplashZ")
 hs.loadSpoon("Windows")
 hs.loadSpoon("HotKeys")
-require "User/config_Popup_TranslateSelection";
-require "User/config_UrlDispatcher";
+hs.loadSpoon("PopupTranslateSelection", {
+    hotkeys = {
+        translate_to_ru = {{"left_control", "left_option"}, "z"},
+        translate_to_en = {{"left_control", "left_option"}, "tab"},
+        translate_to_el = {{"left_control", "left_option"}, "g"},
+    }}, true)
+require "config_UrlDispatcher";
 hs.loadSpoon("KSheet")
 hs.loadSpoon("ReloadConfiguration",{ start = true }, true)
-hs.loadSpoon("FadeLogo",{ start = true }, true)
