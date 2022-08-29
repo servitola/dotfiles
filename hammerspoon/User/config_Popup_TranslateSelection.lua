cd73788e0e8ee571a9d24375c19cb917161f331e
servitola <servitola@gmail.com>
2022-08-30 01:11:33 +0300

    [Hammerspoon] Refactor

--- hammerspoon/User/config_Popup_TranslateSelection.lua
+++
@@ -1,7 +0,0 @@
-local wm = hs.webview.windowMasks
-hs.loadSpoon("PopupTranslateSelection", {
-    hotkeys = {
-        translate_to_ru = {{"left_control", "left_option"}, "z"},
-        translate_to_en = {{"left_control", "left_option"}, "tab"},
-        translate_to_el = {{"left_control", "left_option"}, "g"},
-    }}, true)
