db62c3f1ef04596a711cd6473d6ac099ac0be651
servitola <servitola@gmail.com>
2022-08-30 01:59:49 +0300

    [Hammerspoon] Refactor

--- hammerspoon/Spoons/UnsplashZ.spoon/init.lua
+++ hammerspoon/Spoons/UnsplashZ.spoon/init.lua
@@ -1,43 +1,20 @@
---- === UnsplashZ ===
----
---- Use unsplash images as wallpaper
----
---- Download: [https://github.com/Hammerspoon/Spoons/raw/master/Spoons/UnsplashZ.spoon.zip](https://github.com/Hammerspoon/Spoons/raw/master/Spoons/UnsplashZ.spoon.zip)
-
 local obj={}
-obj.__index = obj
 
--- Metadata
-obj.name = "UnsplashZ"
-obj.version = "1.0"
-obj.author = "ashfinal <ashfinal@gmail.com>"
-obj.homepage = "https://github.com/Hammerspoon/Spoons"
-obj.license = "MIT - https://opensource.org/licenses/MIT"
+user_agent_str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4"
+url = [[ /usr/bin/curl 'https://source.unsplash.com/1792x1120/?nature' |  perl -ne ' print "$1" if /href="([^"]+)"/ ' ]]
 
-local function curl_callback(exitCode, stdOut, stdErr)
+local function curl_callback(exitCode, stdOut)
     if exitCode == 0 then
-        obj.task = nil
-        obj.last_pic = hs.http.urlParts(obj.pic_url).lastPathComponent
         local localpath = os.getenv("HOME") .. "/.Trash/" .. hs.http.urlParts(obj.pic_url).lastPathComponent
-
         hs.screen.mainScreen():desktopImageURL("file://" .. localpath)
-    else
-        print(stdOut, stdErr)
     end
 end
 
 function obj:init()
-    local user_agent_str = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_5) AppleWebKit/603.2.4 (KHTML, like Gecko) Version/10.1.1 Safari/603.2.4"
-    obj.pic_url = hs.execute([[ /usr/bin/curl 'https://source.unsplash.com/1792x1120/?nature' |  perl -ne ' print "$1" if /href="([^"]+)"/ ' ]])
-    if obj.last_pic ~= obj.pic_url then
-        if obj.task then
-            obj.task:terminate()
-            obj.task = nil
-        end
-        local localpath = os.getenv("HOME") .. "/.Trash/" .. hs.http.urlParts(obj.pic_url).lastPathComponent
-        obj.task = hs.task.new("/usr/bin/curl", curl_callback, {"-A", user_agent_str, obj.pic_url, "-o", localpath})
-        obj.task:start()
-    end
+    obj.pic_url = hs.execute(url)
+    local localpath = os.getenv("HOME") .. "/.Trash/" .. hs.http.urlParts(obj.pic_url).lastPathComponent
+    hs.task.new("/usr/bin/curl", curl_callback, {"-A", user_agent_str, obj.pic_url, "-o", localpath})
+        :start()
 end
 
 return obj
