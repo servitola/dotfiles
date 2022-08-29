a55a8bee4cbce7dc478f04b15b7077d132cc3a92
servitola <servitola@gmail.com>
2022-08-29 15:05:52 +0300

    [Hammerspoon] Add adobe website to url filter

--- hammerspoon/User/config_UrlDispatcher.lua
+++ hammerspoon/User/config_UrlDispatcher.lua
@@ -5,11 +5,12 @@ local Firefox = "org.mozilla.firefox"
 spoon.SpoonInstall:andUse("URLDispatcher", {
     config = {
         url_patterns = {
-            {"https?://zoom.us/j/", Zoom},
-            {"https?://%w+.zoom.us/j/", Zoom},
-            {"https?://%w+.spotwa", Zoom},
-            {".*spotwa.*", Safari}, 
-            {".*ctrader.*", Safari}
+            { "https?://zoom.us/j/", Zoom },
+            { "https?://%w+.zoom.us/j/", Zoom },
+            { "https?://%w+.spotwa", Zoom },
+            { ".*spotwa.*", Safari }, 
+            { ".*ctrader.*", Safari },
+            { "xd.adobe.com.*", Safari }
         },
         --url_redir_decoders = {{"Fix broken Preview anchor URLs", "%%23", "#", false, "Preview"}},
         default_handler = Firefox
