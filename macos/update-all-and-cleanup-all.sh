d66b4e8ca491bcdf8bfac5a2312e125e27ac399a
servitola <servitola@gmail.com>
2022-09-03 22:57:53 +0300

    [Update script] improve

--- macos/update-all-and-cleanup-all.sh
+++ macos/update-all-and-cleanup-all.sh
@@ -1,11 +1,14 @@
 #!/usr/bin/env bash
+RED='\033[0;31m'
+NOCOLOR='\033[0m'
+
 sudo -v
 
-echo "==> Updating XCode tools"
+echo "${RED}==> Updating XCode tools${NOCOLOR}"
 sudo softwareupdate -i -a
 xcode-select --install
 
-echo "==> Updating all from Homebrew"
+echo "${RED}==> Updating all from Homebrew${NOCOLOR}"
 rm -rf "brew --cache"
 brew cu --all -y -q
 brew update
@@ -16,16 +19,16 @@ brew doctor
 omz update
 brew bundle dump --force --file=~/projects/dotfiles/homebrew/.brewfile
 
-echo "==> Update tldr caches"
+echo "${RED}==> Update tldr caches${NOCOLOR}"
 tldr --update
 
-npm config set fund false --global
+npm config set fund false --location=global
 
-echo "==> Flashing DNS cache"
+echo "${RED}==> Flashing DNS cache${NOCOLOR}"
 dscacheutil -flushcache
 killall -HUP mDNSResponder
 
-echo "==> Cleaning caches"
+echo "${RED}==> Cleaning caches${NOCOLOR}"
 setopt rm_star_silent
 rm -rf ~/Library/Caches/*
 rm -rf /Library/Caches/*
