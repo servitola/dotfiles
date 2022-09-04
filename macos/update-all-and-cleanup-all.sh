50a30460df4a96772340e0e7cf31397d81c84327
servitola <servitola@gmail.com>
2022-09-04 11:24:08 +0300

    [Zsh Fix tldr caches removing. Refactor

--- macos/update-all-and-cleanup-all.sh
+++ macos/update-all-and-cleanup-all.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
-RED='\033[0;31m'
-NOCOLOR='\033[0m'
+H1='\033[0;31m==>'
+H1_END='\033[0m'
 
 sudo -v
 
-echo "${RED}==> Updating XCode tools${NOCOLOR}"
+echo "${H1} Updating XCode tools ${H1_END}"
 sudo softwareupdate -i -a
 xcode-select --install
 
-echo "${RED}==> Updating all from Homebrew${NOCOLOR}"
+echo "${H1} Updating all from Homebrew ${H1_END}"
 rm -rf "brew --cache"
 brew cu --all -y -q
 brew update
@@ -19,20 +19,21 @@ brew doctor
 omz update
 brew bundle dump --force --file=~/projects/dotfiles/homebrew/.brewfile
 
-echo "${RED}==> Update tldr caches${NOCOLOR}"
-tldr --update
-
+echo "${H1} Configure NPM ${H1_END}"
 npm config set fund false --location=global
 
-echo "${RED}==> Flashing DNS cache${NOCOLOR}"
+echo "${H1} Flashing DNS cache ${H1_END}"
 dscacheutil -flushcache
 killall -HUP mDNSResponder
 
-echo "${RED}==> Cleaning caches${NOCOLOR}"
+echo "${H1} Cleaning caches ${H1_END}"
 setopt rm_star_silent
 rm -rf ~/Library/Caches/*
 rm -rf /Library/Caches/*
 rm -rf ~/Library/Developer/Xcode/DerivedData/*
 setopt no_rm_star_silent
+
+echo "${H1} Update tldr caches ${H1_END}"
+tldr --update
 
 reload
