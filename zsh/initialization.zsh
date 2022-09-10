c620f871ffb8ddfc3324986d512ec7bad1da8964
servitola <servitola@gmail.com>
2022-09-10 23:09:17 +0300

    [Zsh] Refactor android exports

--- zsh/initialization.zsh
+++ zsh/initialization.zsh
@@ -6,15 +6,17 @@ export TERM="xterm-256color"
 export EDITOR='nano'
 
 # BASH
-export PATH=$PATH:~/bin:/usr/local/bin
+export PATH=$PATH:~/bin
+export PATH=$PATH:/usr/local/bin
 export PATH=$PATH:/usr/local/sbin
+
 export PATH=$PATH:/usr/local/share/dotnet/x64
 export PATH=$PATH:~/.dotnet/tools
 export PATH="$PATH:/usr/local/opt/sqlite/bin"
 export ZSH=/Users/servitola/.oh-my-zsh
 
 # ANDROID
-export ANDROID_HOME="~/Library/Android/sdk"
+export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
