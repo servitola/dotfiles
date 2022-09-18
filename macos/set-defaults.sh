13c0fb4ffbd5cd14620c4c63d1efce3d796d80d4
servitola <servitola@gmail.com>
2022-09-18 21:31:39 +0300

    [MacOs] Replace comments with write to console

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -2,73 +2,67 @@
 
 sudo -v
 
-# close any open System Preferences panes, to prevent them from overriding
+echo "Close any open System Preferences panes, to prevent them from overriding"
 osascript -e 'tell application "System Preferences" to quit'
 
-# Disable the sound effects on boot
+echo "Disable the sound effects on boot"
 sudo nvram SystemAudioVolume=" "
 
-# Disable Siri and remove the menu bar icon
+echo "Disable Siri and remove the menu bar icon"
 launchctl unload -w /System/Library/LaunchAgents/com.apple.Siri.plist 2> /dev/null
 defaults write com.apple.Siri StatusMenuVisible -int 0
 defaults write com.apple.Siri UserHasDeclinedEnable -int 1
 
-# Disable the “Are you sure you want to open this application?” dialog
+echo "Disable the “Are you sure you want to open this application?” dialog"
 sudo spctl --master-disable
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Disable the “Are you sure you want to open this application?” dialog
-defaults write com.apple.LaunchServices LSQuarantine -bool false
-
-# Turn Off .dmg Verify
+echo "Turn Off .dmg Verify"
 defaults write com.apple.frameworks.diskimages skip-verify -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
 defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
 
-# Save to disk (not to iCloud) by default
+echo "Save to disk (not to iCloud) by default"
 defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
 
-# Add message to Login Windows
+echo "Add message to Login Windows"
 sudo defaults write /Library/Preferences/com.apple.loginwindow LoginwindowText  "Found this computer? Please contact me at: servitola@gmail.com"
 
-# Set highlight color to green
+echo "Set highlight color to green"
 defaults write NSGlobalDomain AppleHighlightColor -string "0.764700 0.976500 0.568600"
 
-# Disable the “Are you sure you want to open this application?” dialog
+echo "Disable the “Are you sure you want to open this application?” dialog"
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
-# Disable shadow in screenshots
+echo "Disable shadow in screenshots"
 defaults write com.apple.screencapture disable-shadow -bool true
 
-# Set Dock to the right
+echo "Set Dock to the right"
 defaults write com.apple.dock "orientation" -string "right"
 
-# Dim hidden apps
+echo "Dim hidden apps"
 defaults write com.apple.dock showhidden -bool true
 
-# Minimize windows into their application’s icon
+echo "Minimize windows into their application’s icon"
 defaults write com.apple.dock minimize-to-application -bool true
 
-# Don’t automatically rearrange Spaces based on most recent use
+echo "Don’t automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
 
-# Don’t show recent applications in Dock
+echo "Don’t show recent applications in Dock"
 defaults write com.apple.dock show-recents -bool false
 
-# Save to disk (not to iCloud) by default
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
-
-# Always open everything in Finder's list view. This is important.
+echo "Always open everything in Finder's list view. This is important."
 defaults write com.apple.Finder FXPreferredViewStyle Nlsv
 
-# Show the ~/Library folder.
+echo "Show the ~/Library folder."
 chflags nohidden ~/Library
 
 ###############################################################################
-# Finder                                                                      #
+echo "Finder"                                                                 #
 ###############################################################################
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
+echo "Set the Finder prefs for showing a few different volumes on the Desktop."
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
 
@@ -76,18 +70,18 @@ defaults write com.apple.finder ShowTabView -bool true
 defaults write com.apple.finder ShowPathbar -bool true
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# When performing a search, search the current folder by default
+echo "When performing a search, search the current folder by default"
 defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
 
-# Avoid creating .DS_Store files on network volumes
+echo "Avoid creating .DS_Store files on network volumes"
 defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
 
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+echo "Use list view in all Finder windows by default"
+echo "Four-letter codes for the other view modes: 'icnv', 'clmv', 'Flwv'"
 defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Expand the following File Info panes:
-# “General”, “Open with”, and “Sharing & Permissions”
+echo "Expand the following File Info panes:"
+echo "'General', 'Open with', and 'Sharing & Permissions'"
 defaults write com.apple.finder FXInfoPanesExpanded -dict \
   General -bool true \
   OpenWith -bool true \
@@ -95,38 +89,38 @@ defaults write com.apple.finder FXInfoPanesExpanded -dict \
 
 
 ###############################################################################
-# Activity Monitor                                                            #
+echo "Activity Monitor"                                                       #
 ###############################################################################
 
-# Show the main window when launching Activity Monitor
+echo "Show the main window when launching Activity Monitor"
 defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
 
-# Visualize CPU usage in the Activity Monitor Dock icon
+echo "Visualize CPU usage in the Activity Monitor Dock icon"
 defaults write com.apple.ActivityMonitor IconType -int 5
 
-# Show all processes in Activity Monitor
+echo "Show all processes in Activity Monitor"
 defaults write com.apple.ActivityMonitor ShowCategory -int 0
 
-# Sort Activity Monitor results by CPU usage
+echo "Sort Activity Monitor results by CPU usage"
 defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
 defaults write com.apple.ActivityMonitor SortDirection -int 0
 
 ###############################################################################
-# Photos                                                                      #
+echo "Photos"                                                                 #
 ###############################################################################
 
-# Prevent Photos from opening automatically when devices are plugged in
+echo "Prevent Photos from opening automatically when devices are plugged in"
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
 ###############################################################################
-# Keyboard																	  #
+echo "Keyboard"                           																	  #
 ###############################################################################
 
-# Enable full keyboard access for all controls
-# (e.g. enable Tab in modal dialogs)
+echo "Enable full keyboard access for all controls"
+echo "(e.g. enable Tab in modal dialogs)"
 defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
 
-# Add Language Layouts: en, ru, el
+echo "Add Language Layouts: en, ru, el"
 defaults delete com.apple.HIToolbox AppleEnabledInputSources
 defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict>
 			<key>InputSourceKind</key>
@@ -154,11 +148,11 @@ defaults write com.apple.HIToolbox AppleEnabledInputSources -array-add '<dict>
 		</dict>'
 
 ###############################################################################
-# Safari		                                                              #
+echo "Safari"		                                                              #
 ###############################################################################
 
-# Prevent Safari from opening ‘safe’ files automatically after downloading
+echo "Prevent Safari from opening ‘safe’ files automatically after downloading"
 defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
 
-# Remove useless icons from Safari’s bookmarks bar
+echo "Remove useless icons from Safari’s bookmarks bar"
 defaults write com.apple.Safari ProxiesInBookmarksBar "()"
