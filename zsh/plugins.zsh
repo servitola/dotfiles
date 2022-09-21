2a2e382fc14b5549104e90cb024e788797ec582b
servitola <servitola@gmail.com>
2022-09-21 22:59:26 +0300

    [Zsh] Add web-search plugin

--- zsh/plugins.zsh
+++ zsh/plugins.zsh
@@ -16,4 +16,5 @@ plugins=(
   last-working-dir # automatically jumps into last used working directory
   zsh-navigation-tools # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/zsh-navigation-tools
   ohmyzsh-full-autoupdate  # updates oh-my-zsh plugins
+  web-search # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/web-search
 )
