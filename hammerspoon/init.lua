fc218472be5cda8b0e16119de194a10717991a2f
servitola <servitola@gmail.com>
2022-09-22 19:00:52 +0300

    [Hammerspoon] Refactor

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,18 +8,5 @@ hs.loadSpoon("KSheet") -- shortcuts cheatsheet
 hs.loadSpoon("PopupTranslateSelection") -- translate selected text
 hs.loadSpoon("HotKeys") -- all hotkeys
 require "config_UrlDispatcher"; -- open urls in different browsers
-
-
-appwatcher = hs.application.watcher.new(function(appName, eventType, appObject)
- if eventType == hs.application.watcher.activated then
-    if(appObject:bundleID() == "ru.keepcoder.Telegram") then
-      hs.keycodes.setLayout("Russian – PC")
-    else
-      hs.keycodes.setLayout("ABC")
-    end
-  end
-end)
-
-appwatcher:start()
-
-hs.pathwatcher.new(hs.configdir, hs.reload):start() -- reload Hammerspoon config on change
+require "set_language_on_app_focused";
+require "reload_hammerspoon_on_script_changed"
