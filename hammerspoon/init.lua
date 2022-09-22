29500f4647abfc515e2a1349775fcea0945f4448
servitola <servitola@gmail.com>
2022-09-22 08:01:43 +0300

    [Hammerspoon] Keyboard pilot replacing

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,6 @@
 hs.console.clearConsole()
 hs.console.consoleFont({ name = 'JetBrainsMono Nerd Font Mono', size = 11.0 })
+hs.keycodes.setLayout("ABC")
 
 hs.loadSpoon("UnsplashZ") -- download new wallpaper
 hs.loadSpoon("Windows") -- window management
@@ -7,5 +8,18 @@ hs.loadSpoon("KSheet") -- shortcuts cheatsheet
 hs.loadSpoon("PopupTranslateSelection") -- translate selected text
 hs.loadSpoon("HotKeys") -- all hotkeys
 require "config_UrlDispatcher"; -- open urls in different browsers
+
+
+appwatcher = hs.application.watcher.new(function(appName, eventType, appObject)
+ if eventType == hs.application.watcher.activated then
+    if(appObject:bundleID() == "ru.keepcoder.Telegram") then
+      hs.keycodes.setLayout("Russian – PC")
+    else
+      hs.keycodes.setLayout("ABC")
+    end
+  end
+end)
+
+appwatcher:start()
 
 hs.pathwatcher.new(hs.configdir, hs.reload):start() -- reload Hammerspoon config on change
