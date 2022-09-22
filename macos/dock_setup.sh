8dc810d52cafdf3cdd8c703fc69d4d5d8744e6fb
servitola <servitola@gmail.com>
2022-09-22 21:05:22 +0300

    [MacOS] Setup Dock

--- macos/dock_setup.sh
+++ macos/dock_setup.sh
@@ -1,25 +1,14 @@
 #!/bin/zsh
-
-# The purpose of this component
-# is to build the Dock, one app at a time.
-# Dock preferences are located in components/osx/preferences/Dock
-
-echo_title_update "Dock"
-
 dockutil --no-restart --remove all
 
-dockApps=(
-
-  )
+dockApps=( )
 
 for i in "${dockApps[@]}"
 do
   dockutil --no-restart --add $i
 done
 
-dockutil --add '' --type spacer --section apps --after Spotify
-dockutil --add '' --type spacer --section apps --after Safari
-dockutil --add '' --type spacer --section apps --after Franz
+dockutil --add '' --type spacer --section apps --after Finder
 
 dockutil --no-restart --add "~/Downloads" --view list --display folder --sort dateadded
 
