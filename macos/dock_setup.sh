502ffb15f0d5d10d2a547f6872a85342fec92261
servitola <servitola@gmail.com>
2022-09-24 23:30:03 +0300

    [MacOs] Remove unnecessary dock icons

--- macos/dock_setup.sh
+++ macos/dock_setup.sh
@@ -8,8 +8,7 @@ do
   dockutil --no-restart --add $i
 done
 
-dockutil --add '' --type spacer --section apps --after Finder
-
-dockutil --no-restart --add "~/Downloads" --view list --display folder --sort dateadded
+# dockutil --add '' --type spacer --section apps --after Finder
+# dockutil --no-restart --add "~/Downloads" --view list --display folder --sort dateadded
 
 killall Dock
