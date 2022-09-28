0954e6044945753c61baf552da628a3e6d2cdc12
servitola <servitola@gmail.com>
2022-09-28 19:32:18 +0300

    [Hammerspoon] Fix Url Dispatcher

--- hammerspoon/config_UrlDispatcher.lua
+++ hammerspoon/config_UrlDispatcher.lua
@@ -1,16 +1,17 @@
 Zoom = "us.zoom.xos"
 Safari = "com.apple.Safari"
 Firefox = "org.mozilla.firefox"
+urlDispatcherSpoon = hs.loadSpoon("URLDispatcher")
 
-hs.loadSpoon("URLDispatcher", {
-    config = {
-        url_patterns = {
-            { "https?://.*zoom.us/j/", Zoom },
-            { ".*spotwa.*", Safari },
-            { ".*ctrader.*", Safari },
-            { "xd.adobe.com.*", Safari }
-        },
-        url_redir_decoders = {{"Fix broken Preview anchor URLs", "%%23", "#", false, "Preview"}},
-        default_handler = Firefox
-    }
-}, true)
+urlDispatcherSpoon.default_handler = "org.mozilla.firefox"
+
+urlDispatcherSpoon.url_patterns = {
+    { "https?://.*zoom.us/j/", Zoom },
+    { ".*spotwa.*", Safari },
+    { ".*ctrader.*", Safari },
+    { "xd.adobe.com.*", Safari }
+  }
+
+urlDispatcherSpoon.url_redir_decoders = {
+    { "Fix broken Preview anchor URLs", "%%23", "#", false, "Preview" },
+  }
