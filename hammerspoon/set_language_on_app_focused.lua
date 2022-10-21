dee3f69650ecde215884f6d61525f9e385c1e32b
servitola <servitola@gmail.com>
2022-10-21 08:24:01 +0300

    [Hammerspoon] Try to fix set_language_on_app_focused
    Sometimes the windows are too fast and not switching properly

--- hammerspoon/set_language_on_app_focused.lua
+++ hammerspoon/set_language_on_app_focused.lua
@@ -1,5 +1,5 @@
 appwatcher = hs.application.watcher.new(function(appName, eventType, appObject)
-  if eventType == hs.application.watcher.activated then
+  if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.unhidden then
      if(appObject:bundleID() == "ru.keepcoder.Telegram") then
        hs.keycodes.setLayout("Russian - Ilya Birman Typography")
      else
