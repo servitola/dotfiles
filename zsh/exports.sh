350ec475dd23fd5c4701e245c6467bbebfe8fb36
servitola <servitola@gmail.com>
2023-03-16 11:33:57 +0200

    [Zsh] Add Android build-tools 33.0.2 to export
    (cherry picked from commit 805ddc067c3729d98705b32f0ae9285af560c1d5)

--- zsh/exports.sh
+++ zsh/exports.sh
@@ -35,6 +35,7 @@ export ANDROID_SDK=~/Library/Android/sdk
 export ANDROID_HOME=~/Library/Android/sdk
 export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin
 export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/build-tools/33.0.2
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/platform-tools
 
