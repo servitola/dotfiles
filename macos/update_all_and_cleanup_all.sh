d6c936722cf8e44dee9764deb8d7f0b79e7803cb
servitola <servitola@gmail.com>
2023-11-10 14:17:10 +0200

    [cleanining] Remove all iOS simulators logs

--- macos/update_all_and_cleanup_all.sh
+++ macos/update_all_and_cleanup_all.sh
@@ -42,6 +42,8 @@ echo "Cleaning caches"
 setopt rm_star_silent #turn off safe mode
 rm -rfv ~/Library/Caches/*
 rm -rfv /Library/Caches/*
+rm -rfv ~/Library/Logs/**/*.log
+rm -rfv ~/Library/Logs/**/*.log.0
 rm -rfv ~/Library/Developer/Xcode/DerivedData/*
 rm -rfv ~/.local/share/NuGet/v3-cache/*
 echo "Cleaning DS_Store"
