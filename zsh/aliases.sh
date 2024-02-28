2940f172b97a9c9bfe919ee35fe6f7508d421d29
servitola <servitola@gmail.com>
2024-02-29 00:22:09 +0200

    [zsh] Replace obsoleted exa with eza

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -43,13 +43,13 @@ alias wifis="wifi -s"
 alias ports='netstat -vanp tcp'
 alias ports_listeners='lsof -nP -iTCP -sTCP:LISTEN'
 
-# EXA aliases                                                       # ls
-alias l='exa -lbF --git'                                               # list, size, type, git
-alias ll='exa -lbGF --git'                                             # long list
-alias llm='exa -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='exa -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='exa -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-alias lS='exa -1'			                                                  # one column, just names
-alias lt='exa --tree --level=2'
+# EZA aliases                                                       # ls
+alias l='eza -lbF --git'                                               # list, size, type, git
+alias ll='eza -lbGF --git'                                             # long list
+alias llm='eza -lbGF --git --sort=modified'                            # long list, modified date sort
+alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
+alias lS='eza -1'			                                                  # one column, just names
+alias lt='eza --tree --level=2'
 # List only directories
 alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
