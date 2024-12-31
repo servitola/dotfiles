0a6ab6d422730424bc1777309dbe6177b8b02fcd
servitola <servitola@gmail.com>
2024-12-31 19:31:32 +0200

    [telegram] switch to AyuGram

--- hammerspoon/set_language_on_app_focused.lua
+++ hammerspoon/set_language_on_app_focused.lua
@@ -1,7 +1,8 @@
 appwatcher = hs.application.watcher.new(function(appName, eventType, appObject)
   if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.unhidden then
 
-     if(appObject:bundleID() == "ru.keepcoder.Telegram") then
+     bundleId = appObject:bundleID()
+     if(bundleId == "ru.keepcoder.Telegram" or bundleId == "one.ayugram.AyuGramDesktop") then
        hs.keycodes.setLayout("Russian – Ilya Birman Typography")
      else
        hs.keycodes.setLayout("English – Ilya Birman Typography")
