6ea2aac33c901a4565a870e3631bb56bedb58f65
servitola <servitola@gmail.com>
2025-02-16 16:41:24 +0200

    [install] fix make

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -15,7 +15,7 @@ echo "installing powerlevel10k theme to terminal if needed"
 [[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 echo "cloning nx-completion plugin to oh-my-zsh plugins"
-git clone https://github.com/jscutlery/nx-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/nx-completion
+[[ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/nx-completion ]] || git clone https://github.com/jscutlery/nx-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/nx-completion
 
 echo "cloning OhMyZsh-full-autoupdate to oh-my-zsh plugins"
-git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
+[[ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate ]] || git clone https://github.com/Pilaton/OhMyZsh-full-autoupdate.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/ohmyzsh-full-autoupdate
