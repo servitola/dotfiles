24a4c8f06df8b47b34961b8fb3d8d3ecace6d0ea
servitola <servitola@gmail.com>
2025-02-16 19:26:24 +0200

    [zsh] improve setup

--- zsh/setup_zsh.sh
+++ zsh/setup_zsh.sh
@@ -1,18 +1,28 @@
 #!/bin/zsh
 
-echo Make ZSH the default shell environment
-chsh -s $(which zsh)
+echo "Make ZSH the default shell environment"
+if [[ $SHELL != $(which zsh) ]]; then
+    chsh -s $(which zsh)
+fi
+
 echo "Setup zsh and terminal stuff symlinks"
 rm -rf ~/.zshrc
 ln -sfvh ~/projects/dotfiles/zsh/zshrc.sh ~/.zshrc
 rm -rf ~/.zprofile
 ln -sfvh ~/projects/dotfiles/zsh/zprofile.sh ~/.zprofile
 
-echo "installing oh-my-zsh to terminal if needed"
-[[ -d ~/.oh-my-zsh ]] || sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "Installing/updating oh-my-zsh if needed"
+if [[ ! -d ~/.oh-my-zsh ]]; then
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+fi
 
-echo "installing powerlevel10k theme to terminal if needed"
-[[ -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]] || git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo "Installing/updating powerlevel10k theme if needed"
+if [[ ! -d ~/.oh-my-zsh/custom/themes/powerlevel10k ]]; then
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+else
+    echo "Powerlevel10k already installed, updating..."
+    cd ~/.oh-my-zsh/custom/themes/powerlevel10k && git pull
+fi
 
 echo "cloning nx-completion plugin to oh-my-zsh plugins"
 [[ -d ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/nx-completion ]] || git clone https://github.com/jscutlery/nx-completion.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/nx-completion
