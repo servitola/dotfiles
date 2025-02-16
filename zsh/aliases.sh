acabc182a7f4e87c31a16e9aabb959420627aeb5
servitola <servitola@gmail.com>
2025-02-16 18:20:51 +0200

    [zsh] improve eza usages

--- zsh/aliases.sh
+++ zsh/aliases.sh
@@ -48,12 +48,12 @@ alias ports='netstat -vanp tcp'
 alias ports_listeners='lsof -nP -iTCP -sTCP:LISTEN'
 
 # EZA aliases                                                       # ls
-alias l='eza -lbF --git'                                               # list, size, type, git
-alias ll='eza -lbGF --git'                                             # long list
-alias llm='eza -lbGF --git --sort=modified'                            # long list, modified date sort
-alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale'  # all list
-alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale' # all + extended list
-alias lS='eza -1'			                                                  # one column, just names
-alias lt='eza --tree --level=2'
+alias l='eza -lbF --git --icons'                                               # list, size, type, git
+alias ll='eza -lbGF --git --icons'                                             # long list
+alias llm='eza -lbGF --git --sort=modified --icons'                            # long list, modified date sort
+alias la='eza -lbhHigUmuSa --time-style=long-iso --git --color-scale --icons'  # all list
+alias lx='eza -lbhHigUmuSa@ --time-style=long-iso --git --color-scale --icons' # all + extended list
+alias lS='eza -1 --icons'			                                                  # one column, just names
+alias lt='eza --tree --level=2 --icons'
 # List only directories
 alias lsd="ls -lF ${colorflag} | grep --color=never '^d'"
