b86008640b4beed68fe7d704eeffba5c38bfe73a
servitola <servitola@gmail.com>
2025-02-18 20:28:29 +0200

    [youtrack] improve youtrack spoons

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -9,8 +9,10 @@ hs.loadSpoon("GruvboxWallpapers") -- set new wallpaper
 hs.loadSpoon("Windows") -- window management
 hs.loadSpoon("KSheet") -- shortcuts cheatsheet
 hs.loadSpoon("PopupTranslateSelection") -- translate selected text
-hs.loadSpoon("HotKeys") -- all hotkeys
+hs.loadSpoon("YouTrackTicket") -- YouTrack ticket creation
+hs.loadSpoon("YouTrackTasks") -- YouTrack tasks view
 hs.loadSpoon("AudioSwitcher") -- audio device switching
+hs.loadSpoon("HotKeys") -- all hotkeys
 
 require "config_UrlDispatcher"; -- open urls in different browsers
 require "set_language_on_app_focused";
