540aa27cd1bbd389d5d1f92761b06e02ba45c5d5
servitola <servitola@gmail.com>
2025-02-19 20:15:53 +0200

    [p10k] improve init

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -1,15 +1,9 @@
 plugins=(
-  git # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/git
-  dotenv # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/dotenv
   aliases # run 'acs' to see all aliases
   common-aliases # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/common-aliases
   macos # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/macos
-  npm # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/npm
-  pip # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/pip
-  python # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/python
   sudo # press 'esc' twice to run sudo
   Colored-man-pages # colors
-  nx-completion # autocomplete
   dirhistory
   last-working-dir # automatically jumps into last used working directory
   zsh-navigation-tools # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/zsh-navigation-tools
@@ -18,8 +12,14 @@ plugins=(
   zoxide
   fzf
   z
+  git            # Core git integration
 )
 
-# Defer loading non-critical plugins
+# Defer loading of non-critical plugins
 zsh-defer source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 zsh-defer source ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh
+zsh-defer load-plugin dotenv        # Environment variables
+zsh-defer load-plugin npm           # npm completions
+zsh-defer load-plugin pip           # pip completions
+zsh-defer load-plugin python        # python utilities
+zsh-defer load-plugin nx-completion # nx completions
