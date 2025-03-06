492085e1bd82d6199775dca55835b819dc24c41c
servitola <servitola@gmail.com>
2025-03-06 23:32:54 +0200

    [hammerspoon] remove unnecessary parameter

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -3,7 +3,6 @@ hs.console.darkMode(true)
 hs.console.alpha(0.99)
 hs.console.consoleFont({ name = 'JetBrainsMono Nerd Font Mono', size = 11.0 })
 hs.keycodes.setLayout("English - Ilya Birman Typography")
-hs.application.enableSpotlightForNameSearches(true)
 
 hs.loadSpoon("GruvboxWallpapers") -- set new wallpaper
 hs.loadSpoon("Windows") -- window management
