6b2ce54f5c7fd9d5cfbbc001b87333446cecc6b4
servitola <servitola@gmail.com>
2025-03-09 01:02:40 +0200

    [hammerspoon] refactor

--- hammerspoon/Spoons/AudioSwitcher.spoon/init.lua
+++ hammerspoon/Spoons/AudioSwitcher.spoon/init.lua
@@ -1,46 +1,22 @@
 local obj = {}
-obj.__index = obj
-obj.name = "AudioSwitcher"
-obj.version = "1.0"
-obj.author = "servitola"
 
-
-function obj:findAudioDevice(pattern)
-    local devices = hs.audiodevice.allOutputDevices()
-    for _, device in ipairs(devices) do
-        if string.find(device:name():lower(), pattern:lower()) then
-            return device
-        end
-    end
-    return nil
-end
+dofile("./Spoons/AudioSwitcher.spoon/find_audio_device.lua")
+dofile("./Spoons/AudioSwitcher.spoon/switch_to_audio_device_safely.lua")
 
 function obj:switchToExternal()
-    local device = self:findAudioDevice("Scarlett")
-    if device then
-        device:setDefaultOutputDevice()
-    end
+    switchToAudioDevice("Scarlett")
 end
 
 function obj:switchToInternal()
-    local device = self:findAudioDevice("MacBook Pro")
-    if device then
-        device:setDefaultOutputDevice()
-    end
+    switchToAudioDevice("MacBook Pro")
 end
 
 function obj:switchToMarshall()
-    local device = self:findAudioDevice("Marshall")
-    if device then
-        device:setDefaultOutputDevice()
-    end
+    switchToAudioDevice("Marshall")
 end
 
 function obj:switchToBT()
-    local device = self:findAudioDevice("BT")
-    if device then
-        device:setDefaultOutputDevice()
-    end
+    switchToAudioDevice("BT")
 end
 
 return obj
