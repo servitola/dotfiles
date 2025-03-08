515fc76c599b258c62b9a888e12cc37c4cc663b2
servitola <servitola@gmail.com>
2025-03-09 00:52:01 +0200

    [window] improve windows management

---
+++ hammerspoon/Spoons/Windows.spoon/config.lua
@@ -0,0 +1,16 @@
+right_side_app_titles = {}
+bottom_side_app_titles = {"IINA", "Transmission"}
+
+margin = 0.005
+animation_duration = 0.1
+horizontal_line = 0.70
+vertical_line = 0.73
+
+-----------------------------------------
+
+top_bar_height = 25
+spacing = margin * 2
+leftX = margin
+topY = margin
+rightX = 1 - margin / 1.5
+bottomY = 1 - margin * 1.5
