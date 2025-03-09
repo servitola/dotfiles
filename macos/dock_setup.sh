c83ea55e862f0a8ababe5ec918ef2df3b71e3fda
servitola <servitola@gmail.com>
2025-03-09 12:15:52 +0200

    [zsh] improve logs

--- macos/dock_setup.sh
+++ macos/dock_setup.sh
@@ -23,14 +23,11 @@ dockutil --no-restart --remove all
 echo "Set LaunchPad to Dock"
 dockApps=(
   "/Applications/LaunchPad.app"
- )
+)
 
 for i in "${dockApps[@]}"
 do
   dockutil --no-restart --add $i
 done
-
-# dockutil --add '' --type spacer --section apps --after Finder
-# dockutil --no-restart --add '~/Downloads' --view grid --display folder --sort dateadded
 
 killall Dock
