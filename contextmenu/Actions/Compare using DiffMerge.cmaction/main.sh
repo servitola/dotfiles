58b912e695c5e99ff8ffe8bc8371c9604d8fda23
servitola <servitola@gmail.com>
2025-04-18 10:36:10 +0300

    [contextmenu] remove

--- contextmenu/Actions/Compare using DiffMerge.cmaction/main.sh
+++
@@ -1,6 +0,0 @@
-file1="$1"
-file2="$2"
-# both files or directories
-if [[ (-f "$file1" && -f "$file2") || (-d "$file1" && -d "$file2") ]]; then
-  /Applications/DiffMerge.app/Contents/Resources/diffmerge.sh "$file1" "$file2"
-fi
