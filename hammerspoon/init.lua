d6ad562cf74f5c730a911948c0ebbb8a4cfe52c3
servitola <servitola@gmail.com>
2025-04-28 07:01:11 +0300

    [hammerspoon] Add control + option + y to start youtube video with music streaming

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -12,6 +12,7 @@ hs.loadSpoon("YouTrackTicket") -- YouTrack ticket creation
 hs.loadSpoon("YouTrackTasks") -- YouTrack tasks view
 hs.loadSpoon("BrowserTabOpener") -- YouTube tab opener
 hs.loadSpoon("AudioSwitcher") -- audio device switching
+hs.loadSpoon("YouTubeStream") -- YouTube stream player
 hs.loadSpoon("HotKeys") -- all hotkeys
 
 require "config_UrlDispatcher" -- open urls in different browsers
