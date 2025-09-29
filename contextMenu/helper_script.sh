025bc5d17fd07063ec1cf254eca9765e0998f869
servitola <servitola@gmail.com>
2025-09-29 22:55:37 +0300

    [contextMenu] add install helper script

---
+++ contextMenu/helper_script.sh
@@ -0,0 +1,6 @@
+scripts_dir=~/Library/Application\ Scripts/net.langui.ContextMenuHelper
+mkdir -p "$scripts_dir"
+script_content=$'#!/bin/sh\npkill -nf ScriptMonitor\nexec "$@"'
+helper_path="$scripts_dir/contextmenu.sh"
+echo "$script_content" > "$helper_path"
+chmod +x "$helper_path"
