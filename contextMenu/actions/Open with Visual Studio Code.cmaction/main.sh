8c71d5575e62ffa20aa7e53d4f974f7ff6e984e8
servitola <servitola@gmail.com>
2025-09-30 12:15:23 +0300

    [contextMenu] save actions

---
+++ contextMenu/actions/Open with Visual Studio Code.cmaction/main.sh
@@ -0,0 +1,10 @@
+cmd1='/usr/local/bin/code'
+cmd2='/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code'
+if [ -f "$cmd1" ]; then
+  cmd="$cmd1"
+elif [ -f "$cmd2" ]; then
+  cmd="$cmd2"
+fi
+if ! [[ -z "$cmd" ]]; then
+  "$cmd" "$@"
+fi
