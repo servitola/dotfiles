cf3176a3faaec1c6c2d6d1c62f18ccc9e666ff38
servitola <servitola@gmail.com>
2025-10-30 23:38:37 +0200

    [dotfiles] show random askii art from internet on macos updated

---
+++ zsh/bin/random_ascii.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+#
+# Fetch and display random ASCII art from asciiartfarts.com
+#
+# Usage: Just run the script
+
+echo
+
+curl -s "http://www.asciiartfarts.com/random.cgi" | \
+  sed -n '/<pre>/,/<\/pre>/p' | \
+  sed 's/<[^>]*>//g' | \
+  sed "s/&lt;/</g; s/&gt;/>/g; s/&amp;/\&/g; s/&quot;/\"/g; s/&#39;/'/g; s/&nbsp;/ /g" | \
+  sed '/^$/d' | \
+  tail -n +7
