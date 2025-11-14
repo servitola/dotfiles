5bfcfad2e5717e1b664561dac0b261c6297cb3ab
servitola <servitola@gmail.com>
2025-11-14 18:18:27 +0200

    [zsh] return colored-man-pages

--- zsh/plugins.sh
+++ zsh/plugins.sh
@@ -1,8 +1,10 @@
 plugins=(
-  git            # Core git integration
-
+    git
+    colored-man-pages
+    npm
 )
 
+#
 #   fzf            # Fuzzy finder
 
 # Defer loading of non-critical plugins
@@ -16,19 +18,19 @@ plugins=(
 # zsh-defer load-plugin aliases         # alias management
 # zsh-defer load-plugin common-aliases  # common aliases
 # zsh-defer load-plugin macos           # macOS specific features
-# # zsh-defer load-plugin sudo            # ESC ESC to add sudo
-# zsh-defer load-plugin colored-man-pages # colored man pages
+# zsh-defer load-plugin sudo            # ESC ESC to add sudo
 # zsh-defer load-plugin dirhistory      # directory navigation
 # zsh-defer load-plugin last-working-dir # restore last directory
 # zsh-defer load-plugin web-search      # web search from terminal
 # zsh-defer load-plugin ohmyzsh-full-autoupdate  # updates oh-my-zsh plugins
 # zsh-defer load-plugin zsh-navigation-tools # https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins/zsh-navigation-tools
-# # Define the plugin loading function
-# function load-plugin() {
-#   local plugin=$1
-#   if [[ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]]; then
-#     source $ZSH/plugins/$plugin/$plugin.plugin.zsh
-#   elif [[ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]]; then
-#     source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
-#   fi
-# }
+
+# Define the plugin loading function
+function load-plugin() {
+  local plugin=$1
+  if [[ -f $ZSH/plugins/$plugin/$plugin.plugin.zsh ]]; then
+    source $ZSH/plugins/$plugin/$plugin.plugin.zsh
+  elif [[ -f $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh ]]; then
+    source $ZSH_CUSTOM/plugins/$plugin/$plugin.plugin.zsh
+  fi
+}
