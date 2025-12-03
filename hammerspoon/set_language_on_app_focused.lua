a2e1a262d938f815defbc1c471732f07dde9e75b
servitola <servitola@gmail.com>
2025-12-03 08:18:06 +0200

    [hammerspoon] improve language switching on app focused with fighting against race conditions

--- hammerspoon/set_language_on_app_focused.lua
+++ hammerspoon/set_language_on_app_focused.lua
@@ -1,14 +1,39 @@
-appwatcher = hs.application.watcher.new(function(appName, eventType, appObject)
-  if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.unhidden then
+local lastApp = nil
+local switchTimer = nil
 
-      bundleId = appObject:bundleID()
-      if(bundleId == "ru.keepcoder.Telegram"
-         or bundleId == "one.ayugram.AyuGramDesktop")
-      then
-        hs.keycodes.setLayout("Ru Birman")
-      else
-        hs.keycodes.setLayout("En Birman")
-      end
-  end
+local function setLanguageForApp(bundleId, layout)
+    if switchTimer then
+        switchTimer:stop()
+    end
+
+    switchTimer = hs.timer.doAfter(0.05, function()
+        if lastApp == bundleId then return end
+        lastApp = bundleId
+
+        hs.keycodes.setLayout(layout)
+
+        hs.timer.doAfter(0.02, function()
+            local currentLayout = hs.keycodes.currentLayout()
+            if currentLayout ~= layout then
+                hs.keycodes.setLayout(layout)
+            end
+        end)
+    end)
+end
+
+appwatcher = hs.application.watcher.new(function(appName, eventType, appObject)
+    if eventType == hs.application.watcher.activated or eventType == hs.application.watcher.unhidden then
+        bundleId = appObject:bundleID()
+        if bundleId == "ru.keepcoder.Telegram" or bundleId == "one.ayugram.AyuGramDesktop" then
+            setLanguageForApp(bundleId, "Ru Birman")
+        else
+            setLanguageForApp(bundleId, "En Birman")
+        end
+    elseif eventType == hs.application.watcher.deactivated then
+        bundleId = appObject:bundleID()
+        if bundleId == "ru.keepcoder.Telegram" or bundleId == "one.ayugram.AyuGramDesktop" then
+            lastApp = nil
+        end
+    end
 end)
 appwatcher:start()
