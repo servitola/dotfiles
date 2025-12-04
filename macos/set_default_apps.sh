4aea3bb210f80151b702f7fc18fa596243a3b5b3
servitola <servitola@gmail.com>
2025-12-04 07:39:54 +0200

    [macos] open .keylayout files in vscode always

--- macos/set_default_apps.sh
+++ macos/set_default_apps.sh
@@ -101,6 +101,7 @@ duti -s com.microsoft.VSCode Makefile all
 duti -s com.microsoft.VSCode .settings all
 duti -s com.microsoft.VSCode .gitignore all
 duti -s com.microsoft.VSCode .opml all
+duti -s com.microsoft.VSCode .keylayout all
 
 echo "Setting audio/video player"
 duti -s com.colliderli.iina public.audio all
