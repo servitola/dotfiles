1aa843d49eeba9bb1cbcece5ae916bd4ea32ee68
servitola <servitola@gmail.com>
2025-12-08 13:43:32 +0200

    [hotkeys] add control and command buttons docs

---
+++ hammerspoon/Spoons/HotKeys.spoon/layouts/command.lua
@@ -0,0 +1,12 @@
+return {
+--    ╭—————╮
+--    │  ⌘  │    command modifier
+--    ╰—————╯
+--
+--         karabiner: no
+--
+--——————— chord ┬ en | ru | el ┬
+--          ⌘   │      ⌘       │
+--       ⌘click │              │ 🌐 — open in new tab but stay on current page
+--              ┴              ┴
+}
