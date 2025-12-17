7936509c299e9e46c75c648d3053ed416e74193c
servitola <servitola@gmail.com>
2025-12-17 07:50:11 +0200

    [hotkeys] improve docs

--- hammerspoon/Spoons/HotKeys.spoon/layout/60%/v.lua
+++ hammerspoon/Spoons/HotKeys.spoon/layout/60%/v.lua
@@ -5,40 +5,40 @@ return {
 --
 --         karabiner: ⇪v → F19
 --
---————————— chord ┬ en | ru | el ┬
---           v    │ v    м    ω  │
---————————————————┼——————————————┼——————————————————————————————————
---           ⇪v   │     F19      │
-{ chord =  "F19",                  app = "Yandex" },
---           ⇧v   │ V    М    Ω  │
---           ⌃v   │              │ Terminal — paste
---           ⌥v   │ ↓    ↓    ↓  │
---           ⌘v   │              │  — paste
---                │              │ Music — paste song information or artwork
---————————————————┼——————————————┼——————————————————————————————————
---          ⇪⇧v   │     ⇧F19     │ ↓
-{ chord = "⇧F19",                  app = "Google Chrome" },
---          ⇪⌃v   │     ⌃F19     │ ↓
-{ chord = "⌃F19",                  fn = "vpn.toggle_globalprotect" },
---          ⇪⌥v   │     ⌥F19     │ ???
---          ⇪⌘v   │     ⌘F19     │ ↓
-{ chord = "⌘F19",                  fn = "browser_search_selected" },
---          ⇧⌃v   │              │ Terminal — used sometimesž
---          ⇧⌥v   │ ˇ    ˇ    ˇ  │
---          ⇧⌘v   │              │ Mail — paste text into email as quotation
-{ chord =  "⌃⌥v",                  fn = "browser_youtube" },
---          ⌃⌘v   │              │ ???
---          ⌥⌘v   │              │ ℝ — extract variable
---————————————————┼——————————————┼——————————————————————————————————
---         ⇪⇧⌃v   │              │
---         ⇪⇧⌥v   │              │
---         ⇪⇧⌘v   │              │
---         ⇪⌃⌥v   │              │
---         ⇪⌃⌘v   │              │
---         ⇪⌥⌘v   │              │
---         ⇧⌃⌥v   │              │
---         ⇧⌃⌘v   │              │
---         ⇧⌥⌘v   │              │
---         ⌃⌥⌘v   │              │
---                ┴              ┴
+--————————— chord ┬ en | ru | el ┬ ┬
+--           v    │ v    м    ω  │⋅│
+--————————————————┼——————————————┼—┼——————————————————————————————————
+--           ⇪v   │     F19      │k│ ↓
+{ chord =  "F19",                    app = "Yandex" },
+--           ⇧v   │ V    М    Ω  │⋅│
+--           ⌃v   │              │⋅│ Terminal — paste
+--           ⌥v   │ ↓    ↓    ↓  │⋅│
+--           ⌘v   │              │⋅│  — paste
+--                │              │⋅│ Music — paste song information or artwork
+--————————————————┼——————————————┼—┼——————————————————————————————————
+--          ⇪⇧v   │     ⇧F19     │k│ ↓
+{ chord = "⇧F19",                    app = "Google Chrome" },
+--          ⇪⌃v   │     ⌃F19     │k│ ↓
+{ chord = "⌃F19",                    fn = "vpn.toggle_globalprotect" },
+--          ⇪⌥v   │     ⌥F19     │k│ ???
+--          ⇪⌘v   │     ⌘F19     │k│ ↓
+{ chord = "⌘F19",                    fn = "browser_search_selected" },
+--          ⇧⌃v   │              │⋅│ Terminal — used sometimesž
+--          ⇧⌥v   │ ˇ    ˇ    ˇ  │⋅│
+--          ⇧⌘v   │              │⋅│ Mail — paste text into email as quotation
+{ chord =  "⌃⌥v",                    fn = "browser_youtube" },
+--          ⌃⌘v   │              │ │ ???
+--          ⌥⌘v   │              │⋅│ ℝ — extract variable
+--————————————————┼——————————————┼—┼——————————————————————————————————
+--         ⇪⇧⌃v   │              │ │
+--         ⇪⇧⌥v   │              │ │
+--         ⇪⇧⌘v   │              │ │
+--         ⇪⌃⌥v   │              │ │
+--         ⇪⌃⌘v   │              │ │
+--         ⇪⌥⌘v   │              │ │
+--         ⇧⌃⌥v   │              │ │
+--         ⇧⌃⌘v   │              │ │
+--         ⇧⌥⌘v   │              │ │
+--         ⌃⌥⌘v   │              │ │
+--                ┴              ┴ ┴
 }
