28afe0e784f1843edb1ad8b18ffd22efc83fde89
servitola <servitola@gmail.com>
2026-03-12 08:21:18 +0200

    [zsh] remove unnecessary

--- zsh/functions.sh
+++ zsh/functions.sh
@@ -8,10 +8,6 @@ tty_red="$(tty_mkbold 31)"
 tty_bold="$(tty_mkbold 39)"
 tty_reset="$(tty_escape 0)"
 
-# function echo() {
-#     printf "${tty_blue}==>${tty_bold} %s${tty_reset}\n" "$(shell_join "$@")"
-# }
-
 function shell_join() {
     local arg
     if [ $# -eq 0 ]; then
