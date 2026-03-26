7d81ac3de9912b28a1ec04c0be66c1ec781edd66
servitola <servitola@gmail.com>
2026-03-26 20:13:35 +0200

    [hammerspoon] refactor audio output notification

--- hammerspoon/Spoons/AudioSwitcher.spoon/switch_to_audio_device_safely.lua
+++ hammerspoon/Spoons/AudioSwitcher.spoon/switch_to_audio_device_safely.lua
@@ -1,9 +1,10 @@
-function switchToAudioDevice(deviceName)
-
+function switchToAudioDevice(deviceName, message)
     local device = findAudioDevice(deviceName)
 
     if device then
         device:setDefaultOutputDevice()
+        hs.notify.new({ title = device:name(), informativeText = "OK" }):send()
+    else
+        hs.notify.new({ title = "⚠️ " .. deviceName .. " not found", informativeText = "warning" }):send()
     end
-
 end
