46b299730cc34feaefe366efb24cff9054b2bc76
servitola <servitola@gmail.com>
2026-03-28 16:08:19 +0200

    [hotkeys] focus playing youtube tab

--- hammerspoon/Spoons/BrowserTabOpener.spoon/init.lua
+++ hammerspoon/Spoons/BrowserTabOpener.spoon/init.lua
@@ -1,6 +1,8 @@
 local obj = {}
 obj.__index = obj
 
+obj._task = nil  -- prevent GC of running hs.task
+
 function obj:openTab(urlPattern)
     local script = string.format([[
         tell application "Yandex"
@@ -36,20 +38,81 @@ function obj:openTab(urlPattern)
         end tell
     ]], urlPattern:gsub('"', '\\"'))
 
-    local success, result, err = hs.osascript.applescript(script)
+    self._task = hs.task.new("/usr/bin/osascript", function(exitCode, stdout, stderr)
+        self._task = nil
+        local result = stdout and stdout:gsub("%s+$", "") or ""
 
-    if not success then
-        hs.logger.new('BrowserTabOpener'):d("AppleScript error: " .. tostring(err))
-    end
+        if exitCode ~= 0 then
+            hs.logger.new('BrowserTabOpener'):d("AppleScript error: " .. tostring(stderr))
+        end
 
-    if success and result == "ok" then
-        -- Tab found and switched — just focus the browser
-    else
-        hs.application.launchOrFocus("Yandex")
-        hs.timer.doAfter(0.1, function()
-            hs.urlevent.openURLWithBundle("https://" .. urlPattern, "ru.yandex.desktop.yandex-browser")
-        end)
-    end
+        if result == "ok" then
+            -- Tab found and switched
+        else
+            hs.application.launchOrFocus("Yandex")
+            hs.timer.doAfter(0.1, function()
+                hs.urlevent.openURLWithBundle("https://" .. urlPattern, "ru.yandex.desktop.yandex-browser")
+            end)
+        end
+    end, {"-e", script})
+    self._task:start()
+end
+
+function obj:focusPlayingTab(urlPattern)
+    local script = string.format([[
+        tell application "Yandex"
+            set firstMatchTab to 0
+            set playingTab to 0
+
+            set tabCounter to 0
+            repeat with t in tabs of front window
+                set tabCounter to tabCounter + 1
+                if URL of t contains "%s" then
+                    if firstMatchTab = 0 then
+                        set firstMatchTab to tabCounter
+                    end if
+                    try
+                        set jsResult to (execute t javascript "String(!document.querySelector('video')?.paused)")
+                        if jsResult = "true" then
+                            set playingTab to tabCounter
+                            exit repeat
+                        end if
+                    end try
+                end if
+            end repeat
+
+            if playingTab > 0 then
+                set active tab index of front window to playingTab
+                activate
+                return "ok"
+            else if firstMatchTab > 0 then
+                set active tab index of front window to firstMatchTab
+                activate
+                return "ok"
+            else
+                return "none"
+            end if
+        end tell
+    ]], urlPattern:gsub('"', '\\"'))
+
+    self._task = hs.task.new("/usr/bin/osascript", function(exitCode, stdout, stderr)
+        self._task = nil
+        local result = stdout and stdout:gsub("%s+$", "") or ""
+
+        if exitCode ~= 0 then
+            hs.logger.new('BrowserTabOpener'):d("AppleScript error: " .. tostring(stderr))
+        end
+
+        if result == "ok" then
+            -- Tab found and switched
+        else
+            hs.application.launchOrFocus("Yandex")
+            hs.timer.doAfter(0.1, function()
+                hs.urlevent.openURLWithBundle("https://youtube.com", "ru.yandex.desktop.yandex-browser")
+            end)
+        end
+    end, {"-e", script})
+    self._task:start()
 end
 
 return obj
