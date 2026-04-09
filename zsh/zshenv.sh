22728f45958fb171d84efc922310f8572ff38f3f
servitola <servitola@gmail.com>
2026-04-09 13:52:03 +0300

    [zsh] fix temporary

--- zsh/zshenv.sh
+++ zsh/zshenv.sh
@@ -1,4 +1,4 @@
 source ~/projects/dotfiles/zsh/exports.sh
 source ~/projects/dotfiles/zsh/exports-path.sh
 source ~/.config/claude_code_settings.sh
-source ~/projects/dotfiles/zsh/aliases.sh
+#source ~/projects/dotfiles/zsh/aliases.sh
