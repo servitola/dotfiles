7420f9b30f175e7f605676041c46d3ef6c6e7319
servitola <servitola@gmail.com>
2026-07-22 07:05:30 +0300

    [homebrew] echo the file that is actually installed
    The script installs minimum_brewfile but announced homebrew/brewfile.

--- homebrew/install_minimum_homebrew_packages.sh
+++ homebrew/install_minimum_homebrew_packages.sh
@@ -1,4 +1,4 @@
 #!/bin/zsh
 
-echo "installing brew packages listed in homebrew/brewfile"
+echo "installing brew packages listed in homebrew/minimum_brewfile"
 brew bundle --file=~/projects/dotfiles/homebrew/minimum_brewfile --verbose
